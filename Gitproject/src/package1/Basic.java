dc9ee150eb864bb19a0046a23d9f456710ed6353
Dinesh <Dinesh@192.168.137.74>
2023-10-30 11:57:20 +0530

    Second commit

--- Gitproject/src/package1/Basic.java
+++ Gitproject/src/package1/Basic.java
@@ -5,6 +5,7 @@ public class Basic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello Java Git");
+       System.out.println("Hello GIt");
 	}
 
 }
